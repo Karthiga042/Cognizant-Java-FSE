8c1b626100fbe5d182352d16d238eaa6bfb3f24f
Karthiga042 <karthigamarimuthu40@gmail.com>
2025-06-28 23:27:08 +0530

    Add JUnit_Basic Testing Exercises to Week 2

---
+++ Week 2/JUnit_Basic Testing Exercises/JunitAAApattern/src/test/java/com/example/CalculatorTest.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    private Calculator calculator;
+
+    // Setup method (runs before each @Test)
+    @Before
+    public void setUp() {
+        calculator = new Calculator();
+        System.out.println("Setup complete.");
+    }
+
+    // Teardown method (runs after each @Test)
+    @After
+    public void tearDown() {
+        System.out.println("Teardown complete.");
+    }
+
+    @Test
+    public void testAddition() {
+        // Arrange
+        int a = 10, b = 5;
+
+        // Act
+        int result = calculator.add(a, b);
+
+        // Assert
+        assertEquals(15, result);
+    }
+
+    @Test
+    public void testSubtraction() {
+        // Arrange
+        int a = 10, b = 5;
+
+        // Act
+        int result = calculator.subtract(a, b);
+
+        // Assert
+        assertEquals(5, result);
+    }
+}
